2e32a062e7f664bc7041bd92ecebf06f29f7b9e5
Taeho <taeho@mhand.net>
2022-02-21 17:28:12 +0900

    :feat Add ownable

--- contracts/SunmiyaNFT.sol
+++ contracts/SunmiyaNFT.sol
@@ -2,12 +2,9 @@
 pragma solidity ^0.5.0;
 
 import './klaytn/token/KIP17/KIP17Token.sol';
+import './klaytn/ownership/Ownable.sol';
 
-contract SunmiyaNFT is KIP17Token {
+contract SunmiyaNFT is KIP17Token, Ownable {
     constructor (string memory name, string memory symbol) public KIP17Token(name, symbol) {
     }
-
-    // function tokenURI(uint256 _tokenId) public pure returns (string memory) {
-    //     return string(abi.encodePacked(baseTokenURI(), Strings.toString(_tokenId)));
-    // }    
 }
