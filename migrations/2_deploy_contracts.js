c3fda3240968eb1fe751a6fae3cb7080e7bf5f9b
Taeho <taeho@mhand.net>
2022-02-21 18:56:17 +0900

    :chore update output folder

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,12 +7,12 @@ module.exports = async function (deployer) {
   // await deployer.deploy(SunmiyaNFT, 'Sunmiya', 'MIYA');
 
   const networkName = process.env.NETWORK_NAME || 'MAINNET';
-  const deployPath = path.resolve(__dirname, '../../', networkName);
+  const deployPath = path.resolve(__dirname, '../output/', networkName);
   const filename = `${SunmiyaNFT.contractName}.json`;
   
   fs.mkdirpSync(deployPath);
   fs.writeFileSync(path.resolve(deployPath, filename), JSON.stringify({
     address: SunmiyaNFT.address,
-    abi: SunmiyaNFT._json.abi
-  }), 'utf8');
+    abi: SunmiyaNFT.abi
+  }, null ,2), 'utf8');
 };
