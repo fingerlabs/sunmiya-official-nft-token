764875fa27bf14122ecb48211ce659c55656378a
Taeho <taeho@mhand.net>
2022-02-21 18:20:53 +0900

    :chore update README

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,18 @@
+const fs = require('fs-extra');
+const path = require('path');
+
 module.exports = async function (deployer) {
   const SunmiyaNFT = artifacts.require('SunmiyaNFT');
-  await deployer.deploy(SunmiyaNFT, 'Sunmiya', 'MIYA');
+  await deployer.deploy(SunmiyaNFT, 'Toshi NFT', 'TOSHI');
+  // await deployer.deploy(SunmiyaNFT, 'Sunmiya', 'MIYA');
+
+  const networkName = process.env.NETWORK_NAME || 'MAINNET';
+  const deployPath = path.resolve(__dirname, '../../', networkName);
+  const filename = `${SunmiyaNFT.contractName}.json`;
+  
+  fs.mkdirpSync(deployPath);
+  fs.writeFileSync(path.resolve(deployPath, filename), JSON.stringify({
+    address: SunmiyaNFT.address,
+    abi: SunmiyaNFT._json.abi
+  }), 'utf8');
 };
